8571525d93761d28b1c00d144b3f99b38bf619b1
AldarGosmc <76616465+AldarGosmc@users.noreply.github.com>
2024-02-06 19:35:17 +0300

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -1,1 +1,34 @@
-# напиши здесь код основного приложения и первого экрана
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (QApplication, QWidget, QVBoxLayout, QPushButton, QLabel)
+
+from instr.py import *  #загружаем переменные из файла instr.py
+from second_win import *
+class MainWin(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.set_appear() # устанавливает, как будет выглядеть окно
+        self.initUI() # создаём и настраиваем графические элементы
+        self.connects() # устанавливает связи между элементами
+        self.show()  # показываем окно
+    def set_appear(self):
+        self.setWindowTitle(txt_title)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
+    def initUI(self):
+        self.hello_text = QLabel(txt_hello)
+        self.insrtuction = QLabel(txt_insrtuction)
+        self.button = QPushDutton(txt_next)
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.hello_text)
+        self.layout.addWidget(self.instruction)
+        self.layout.addWidget(self.button)
+        self.setLayout(self.layout)
+    def next_click(self):
+        self.hide()
+        self.tw = TestWin()
+    def connects(self):
+        self.btn_next.clicked.connect(self.next_click)
+
+app = QApplication([])
+mw = MainWin()
+app.exec()
